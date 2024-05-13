98b109b896f718887c8f3be0d74cb1806506a112
Michele Bamdeg De Chiffre <michele@dechiffre.dk>
2024-05-13 16:33:57 +0200

    Adds jsDocs to Fornitore

--- moduli/fornitore.ts
+++ moduli/fornitore.ts
@@ -3,9 +3,13 @@
  * which can be subscribed to
  */
 export default class Fornitore<T> { 
-    funz: Array<(value: T) => void> = [];
+    private funz: Array<(value: T) => void> = [];
     private lastValue: T | undefined;
 
+    /**
+     * Run it when a new thing happens
+     * @param value new value
+     */
     onEvento(value: T) {
         this.lastValue = value;
         for (let i = 0; i < this.funz.length; i++) {
@@ -14,7 +18,7 @@ export default class Fornitore<T> {
     }
 
     /**
-     * 
+     * Subscribe to changes
      * @param callback 
      * @param runOnSubscribe runs when subscribe, if the event has just happened previously
      */
